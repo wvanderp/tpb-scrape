9c1e860f35d0a3afcb445bc637cc003bfe09c533
wouter van der plas <wouterv.dplas@gmail.com>
2015-07-27 01:18:10 +0200

    adding a way to automate the adding of the tables (not Tested)

--- setup.js
+++ setup.js
@@ -2,6 +2,8 @@ var fs = require("fs");
 var mysql = require('mysql');
 
 var settings = JSON.parse(fs.readFileSync('settings.json', 'utf8'));
+var dbSql = fs.readFileSync('sql/tpb-scrape.sql', 'utf8');
+
 
 var connection = mysql.createConnection({
   host     : settings.hostname,
@@ -12,6 +14,12 @@ var connection = mysql.createConnection({
 
 connection.connect();
 console.log("start")
+console.log("checking database");
+connection.query(dbSql, function(err, rows, fields) {if (err) throw err;});
+console.log("database is good.");
+console.log("table inserted");
+
+console.log("adding id's");
 next(settings.start_id);
 var startDate = Date.now();
 
